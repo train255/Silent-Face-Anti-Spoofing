df7360d81ad434379c32a982dc646a7367fd3e84
Dang Duy Thanh <thanhdd.it@gmail.com>
2021-05-10 15:17:41 +0700

    feat(classify image)

--- test.py
+++ test.py
@@ -6,6 +6,8 @@
 # @Software : PyCharm
 
 import os
+from os.path import isfile, join, exists
+
 import cv2
 import numpy as np
 import argparse
@@ -31,64 +33,73 @@ def check_image(image):
         return True
 
 
-def test(image_name, model_dir, device_id, num_classes):
+def test(model_dir, device_id, num_classes, src_dir, dst_dir, draw_bbox):
     model_test = AntiSpoofPredict(device_id)
     image_cropper = CropImage()
-    image = cv2.imread(SAMPLE_IMAGE_PATH + image_name)
-    # result = check_image(image)
-    # if result is False:
-    #     return
-    image_bbox = model_test.get_bbox(image)
-    # if you have n clasees => prediction = np.zeros((1, n))
-    prediction = np.zeros((1, num_classes))
-    
-    test_speed = 0
-    # sum the prediction from single model's result
-    count_model = 0
-    for model_name in os.listdir(model_dir):
-        h_input, w_input, model_type, scale = parse_model_name(model_name)
-        param = {
-            "org_img": image,
-            "bbox": image_bbox,
-            "scale": scale,
-            "out_w": w_input,
-            "out_h": h_input,
-            "crop": True,
-        }
-        if scale is None:
-            param["crop"] = False
-        img = image_cropper.crop(**param)
-        start = time.time()
-        prediction += model_test.predict(img, os.path.join(model_dir, model_name))
-        count_model = count_model + 1
-        test_speed += time.time()-start
 
-    # draw result of prediction
-    label = np.argmax(prediction)
-    value = prediction[0][label]/count_model
-    if label == 1:
-        print("Image '{}' is Real Face. Score: {:.2f}.".format(image_name, value))
-        result_text = "RealFace Score: {:.2f}".format(value)
-        color = (255, 0, 0)
-    else:
-        print("Image '{}' is Fake Face. Score: {:.2f}.".format(image_name, value))
-        result_text = "FakeFace Score: {:.2f}".format(value)
-        color = (0, 0, 255)
-    print("Prediction cost {:.2f} s".format(test_speed))
-    cv2.rectangle(
-        image,
-        (image_bbox[0], image_bbox[1]),
-        (image_bbox[0] + image_bbox[2], image_bbox[1] + image_bbox[3]),
-        color, 2)
-    cv2.putText(
-        image,
-        result_text,
-        (image_bbox[0], image_bbox[1] - 5),
-        cv2.FONT_HERSHEY_COMPLEX, 0.5*image.shape[0]/1024, color)
+    onlyfiles = [os.path.join(path, name) for path, subdirs, files in os.walk(src_dir) for name in files]
 
-    format_ = os.path.splitext(image_name)[-1]
-    result_image_name = image_name.replace(format_, "_result" + format_)
-    cv2.imwrite(SAMPLE_IMAGE_PATH + result_image_name, image)
+    for file_path in onlyfiles:
+        image_name = os.path.basename(file_path)
+        image = cv2.imread(file_path)
+        image_bbox = model_test.get_bbox(image)
+        print(image_bbox)
+        # if you have n clasees => prediction = np.zeros((1, n))
+        prediction = np.zeros((1, num_classes))
+        
+        test_speed = 0
+        # sum the prediction from single model's result
+        count_model = 0
+        for model_name in os.listdir(model_dir):
+            h_input, w_input, model_type, scale = parse_model_name(model_name)
+            param = {
+                "org_img": image,
+                "bbox": image_bbox,
+                "scale": scale,
+                "out_w": w_input,
+                "out_h": h_input,
+                "crop": True,
+            }
+            if scale is None:
+                param["crop"] = False
+            img = image_cropper.crop(**param)
+            start = time.time()
+            prediction += model_test.predict(img, os.path.join(model_dir, model_name))
+            count_model = count_model + 1
+            test_speed += time.time()-start
+
+        # draw result of prediction
+        label = np.argmax(prediction)
+        value = prediction[0][label]/count_model
+        if label == 1:
+            print("Image '{}' is Real Face. Score: {:.2f}.".format(image_name, value))
+            result_text = "RealFace Score: {:.2f}".format(value)
+            color = (255, 0, 0)
+        else:
+            print("Image '{}' is Fake Face. Score: {:.2f}.".format(image_name, value))
+            result_text = "FakeFace Score: {:.2f}".format(value)
+            color = (0, 0, 255)
+        print("Prediction cost {:.2f} s".format(test_speed))
+
+        if draw_bbox == True:
+            cv2.rectangle(
+                image,
+                (image_bbox[0], image_bbox[1]),
+                (image_bbox[0] + image_bbox[2], image_bbox[1] + image_bbox[3]),
+                color, 2)
+            cv2.putText(
+                image,
+                result_text,
+                (image_bbox[0], image_bbox[1] - 5),
+                cv2.FONT_HERSHEY_COMPLEX, 0.5*image.shape[0]/1024, color)
+
+        dst_path_image = join(dst_dir, str(label))
+        if not exists(dst_path_image):
+            os.makedirs(dst_path_image)
+
+        format_ = os.path.splitext(image_name)[-1]
+        result_image_name = image_name.replace(format_, "_result" + format_)
+        cv2.imwrite(os.path.join(dst_path_image, result_image_name), image)
 
 
 if __name__ == "__main__":
@@ -105,14 +116,22 @@ if __name__ == "__main__":
         default="./resources/anti_spoof_models",
         help="model_lib used to test")
     parser.add_argument(
-        "--image_name",
+        "--src_dir",
         type=str,
         default="image_F1.jpg",
-        help="image used to test")
+        help="source directory")
+    parser.add_argument(
+        "--dst_dir",
+        type=str,
+        default="image_F1.jpg",
+        help="source directory")
     parser.add_argument(
         "--num_classes",
         type=int,
         default=3,
         help="number of classes")
+    parser.add_argument(
+        "--draw_bbox",
+        action='store_true')
     args = parser.parse_args()
-    test(args.image_name, args.model_dir, args.device_id, args.num_classes)
+    test(args.model_dir, args.device_id, args.num_classes, args.src_dir, args.dst_dir, args.draw_bbox)
